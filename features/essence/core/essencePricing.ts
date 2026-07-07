90df0206b75b8a5661adbcaa963e532e88cccd98
Abetar <alexisgomez_96@hotmail.com>
2026-07-07 15:59:09 -0600

    Fixing the types

--- features/essence/core/essencePricing.ts
+++ features/essence/core/essencePricing.ts
@@ -4,5 +4,6 @@ export const ESSENCE_COST: Record<MysticModule, number> = {
   TAROT: 5,
   FORTUNE_COOKIE: 1,
   CLEANSING_RECIPE: 2,
-  CRYSTAL_BALL: 6,
+  CRYSTAL_BALL: 10,
+  GRIMOIRE_RITUAL: 0,
 };
